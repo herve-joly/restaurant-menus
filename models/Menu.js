de3303017d030f37bbbd91e4d41647fd2b141001
= <=>
2023-02-15 20:51:50 +0000

    files setup + Restaurant and Menu model

--- models/Menu.js
+++ models/Menu.js
@@ -1,6 +1,9 @@
-const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const { sequelize } = require("../db");
+const { Sequelize } = require("sequelize");
 
 // TODO - create a Menu model
+const Menu = sequelize.define("Menu", {
+  title: Sequelize.STRING,
+});
 
-module.exports = {Menu};
+module.exports = { Menu };
