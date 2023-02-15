34abca778f3c472e920cc8a7f709cc2328f9dabf
= <=>
2023-02-15 22:07:37 +0000

    can't test for find a restaurant

--- index.test.js
+++ index.test.js
@@ -1,43 +1,47 @@
-const {sequelize} = require('./db')
-const {Restaurant, Menu} = require('./models/index')
-const {
-    seedRestaurant,
-    seedMenu,
-  } = require('./seedData');
+const { where } = require("sequelize");
+const { sequelize } = require("./db");
+const { Restaurant, Menu } = require("./models/index");
+const { seedRestaurant, seedMenu } = require("./seedData");
 
-describe('Restaurant and Menu Models', () => {
-    /**
-     * Runs the code prior to all tests
-     */
-    beforeAll(async () => {
-        // the 'sync' method will create tables based on the model class
-        // by setting 'force:true' the tables are recreated each time the 
-        // test suite is run
-        await sequelize.sync({ force: true });
-    });
+describe("Restaurant and Menu Models", () => {
+  /**
+   * Runs the code prior to all tests
+   */
+  beforeAll(async () => {
+    // the 'sync' method will create tables based on the model class
+    // by setting 'force:true' the tables are recreated each time the
+    // test suite is run
+    await sequelize.sync({ force: true });
+  });
 
-    test('can create a Restaurant', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+  test("can create a Restaurant", async () => {
+    // TODO - write test
+    const testRestaurant = seedRestaurant[0];
+    expect(testRestaurant.name).toEqual("AppleBees");
+  });
 
-    test('can create a Menu', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+  test("can create a Menu", async () => {
+    // TODO - write test
+    const testMenu = seedMenu[0];
+    expect(testMenu.title).toEqual("Breakfast");
+  });
 
-    test('can find Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+  test("can find Restaurants", async () => {
+    // TODO - write test
+    const testRestaurant2 = seedRestaurant[1];
+    const findRestaurant = await Restaurant.findAll({
+      where: { name: "LittleSheep" },
     });
+    expect(findRestaurant).toEqual(testRestaurant2);
+  });
 
-    test('can find Menus', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+  test("can find Menus", async () => {
+    // TODO - write test
+    expect("NO TEST").toEqual("EXPECTED DATA");
+  });
 
-    test('can delete Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
-})
+  test("can delete Restaurants", async () => {
+    // TODO - write test
+    expect("NO TEST").toEqual("EXPECTED DATA");
+  });
+});
